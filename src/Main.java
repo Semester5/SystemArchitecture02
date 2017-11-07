9f6c7e3fc32be00ea1df83536fcbc4f24fe6dada
Julia Schatzmann <julia.schatzmann@students.fhv.at>
2017-11-07 10:29:39 +0100

    added pull-mode

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ import CalcCentroidsFilterPkg.Coordinate;
 import imaging.filter.*;
 import imaging.SourceReader;
 import imaging.TestSink;
+import pmp.interfaces.Readable;
 import pmp.interfaces.Writeable;
 import pmp.pipes.SimplePipe;
 
 import javax.media.jai.PlanarImage;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -21,39 +25,94 @@ public class Main {
         int width = 500;
         int height = 70;
 
-        //PUSH
-        QualityControllFilter qualityFilter = new QualityControllFilter(2,2);
+        ArrayList<Coordinate> expectedCoordinates = new ArrayList<Coordinate>();
+        expectedCoordinates.add(new Coordinate(10,80));
+        expectedCoordinates.add(new Coordinate(73,77));
+        expectedCoordinates.add(new Coordinate(110,80));
+        expectedCoordinates.add(new Coordinate(202,80));
+        expectedCoordinates.add(new Coordinate(265,79));
+        expectedCoordinates.add(new Coordinate(330,81));
+        expectedCoordinates.add(new Coordinate(396,81));
 
-        SimplePipe<ArrayList<Coordinate>> pipeCalcCentroidsToQuality = new SimplePipe<>(qualityFilter);
-        CalcCentroidsFilter calcCentroidsFilter = new CalcCentroidsFilter((Writeable) pipeCalcCentroidsToQuality);
+        int xTolerance = 2;
+        int yTolerance = 2;
 
-        SimplePipe<PlanarImage> pipeSaveToCalcCentroids = new SimplePipe<>((Writeable<PlanarImage>) calcCentroidsFilter);
-        SaveFilter saveFilter = new SaveFilter((Writeable) pipeSaveToCalcCentroids);
+        String mode = "";
+        while(true) {
 
-        SimplePipe<PlanarImage> pipeOpeningToDisplay = new SimplePipe<>((Writeable<PlanarImage>) saveFilter);
-        OpeningFilter openingFilter = new OpeningFilter((Writeable) pipeOpeningToDisplay);
+            System.out.print("Möchten Sie das Programm im Push-Modus [push] oder im Pull-Modus [pull] starten?");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                mode = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-        SimplePipe<PlanarImage> pipeDisplayToOpening = new SimplePipe<>((Writeable<PlanarImage>) openingFilter);
-        DisplayFilter displayFilter3 = new DisplayFilter((Writeable) pipeDisplayToOpening);
+            if ("push".equals(mode.toLowerCase())) {
+                QualityControllFilter qualityFilter = new QualityControllFilter(xTolerance, yTolerance, expectedCoordinates);
 
-        SimplePipe<PlanarImage> pipeMedianToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter3);
-        MedianFilter medianFilter = new MedianFilter((Writeable) pipeMedianToDisplay);
+                SimplePipe<ArrayList<Coordinate>> pipeCalcCentroidsToQuality = new SimplePipe<>(qualityFilter);
+                CalcCentroidsFilter calcCentroidsFilter = new CalcCentroidsFilter((Writeable) pipeCalcCentroidsToQuality);
 
-        SimplePipe<PlanarImage> pipeDisplayToMedian = new SimplePipe<>((Writeable<PlanarImage>) medianFilter);
-        DisplayFilter displayFilter2 = new DisplayFilter((Writeable) pipeDisplayToMedian);
+                SimplePipe<PlanarImage> pipeSaveToCalcCentroids = new SimplePipe<>((Writeable<PlanarImage>) calcCentroidsFilter);
+                SaveFilter saveFilter = new SaveFilter((Writeable) pipeSaveToCalcCentroids);
 
-        SimplePipe<PlanarImage> pipeThresholdToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter2);
-        ThresholdFilter thresholdFilter = new ThresholdFilter((Writeable) pipeThresholdToDisplay);
+                SimplePipe<PlanarImage> pipeTest = new SimplePipe<>((Writeable<PlanarImage>) saveFilter);
+                DisplayFilter displayFilter7 = new DisplayFilter((Writeable) pipeTest);
 
-        SimplePipe<PlanarImage> pipeROIToThreshold = new SimplePipe<>((Writeable<PlanarImage>) thresholdFilter);
-        RegionOfInterestFilter regionOfInterestFilter = new RegionOfInterestFilter(x, y, width, height, (Writeable)  pipeROIToThreshold);
+                SimplePipe<PlanarImage> pipeOpeningToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter7);
+                OpeningFilter openingFilter = new OpeningFilter((Writeable) pipeOpeningToDisplay);
 
-        SimplePipe<PlanarImage> pipeDisplayToROI = new SimplePipe<>((Writeable<PlanarImage>) regionOfInterestFilter);
-        DisplayFilter displayFilter1 = new DisplayFilter((Writeable) pipeDisplayToROI);
+                SimplePipe<PlanarImage> pipeDisplayToOpening = new SimplePipe<>((Writeable<PlanarImage>) openingFilter);
+                DisplayFilter displayFilter3 = new DisplayFilter((Writeable) pipeDisplayToOpening);
 
-        SimplePipe<PlanarImage> pipeSourceToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter1);
-        SourceReader sourceReader = new SourceReader(pipeSourceToDisplay);
+                SimplePipe<PlanarImage> pipeMedianToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter3);
+                MedianFilter medianFilter = new MedianFilter((Writeable) pipeMedianToDisplay);
 
-        sourceReader.run();
+                SimplePipe<PlanarImage> pipeDisplayToMedian = new SimplePipe<>((Writeable<PlanarImage>) medianFilter);
+                DisplayFilter displayFilter2 = new DisplayFilter((Writeable) pipeDisplayToMedian);
+
+                SimplePipe<PlanarImage> pipeThresholdToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter2);
+                ThresholdFilter thresholdFilter = new ThresholdFilter((Writeable) pipeThresholdToDisplay);
+
+                SimplePipe<PlanarImage> pipeROIToThreshold = new SimplePipe<>((Writeable<PlanarImage>) thresholdFilter);
+                RegionOfInterestFilter regionOfInterestFilter = new RegionOfInterestFilter(x, y, width, height, (Writeable) pipeROIToThreshold);
+
+                SimplePipe<PlanarImage> pipeDisplayToROI = new SimplePipe<>((Writeable<PlanarImage>) regionOfInterestFilter);
+                DisplayFilter displayFilter1 = new DisplayFilter((Writeable) pipeDisplayToROI);
+
+                SimplePipe<PlanarImage> pipeSourceToDisplay = new SimplePipe<>((Writeable<PlanarImage>) displayFilter1);
+                SourceReader sourceReader = new SourceReader(pipeSourceToDisplay);
+
+                sourceReader.run();
+                return;
+            } else if ("pull".equals(mode.toLowerCase())) {
+
+                SourceReader sourceReader = new SourceReader();
+                SimplePipe<PlanarImage> pipeROIFromSourceReader = new SimplePipe<>(sourceReader);
+
+                RegionOfInterestFilter regionOfInterestFilter = new RegionOfInterestFilter(x, y, width, height, (Readable) pipeROIFromSourceReader);
+                SimplePipe<PlanarImage> pipeThresholdFromRoi = new SimplePipe<>((Readable) regionOfInterestFilter);
+
+                ThresholdFilter thresholdFilter = new ThresholdFilter((Readable) pipeThresholdFromRoi);
+                SimplePipe<PlanarImage> pipeMedianFromTreshold = new SimplePipe<>((Readable) thresholdFilter);
+
+                MedianFilter medianFilter = new MedianFilter((Readable) pipeMedianFromTreshold);
+                SimplePipe<PlanarImage> pipeOpeningFromMedian = new SimplePipe<>((Readable) medianFilter);
+
+                OpeningFilter openingFilter = new OpeningFilter((Readable) pipeOpeningFromMedian);
+                SimplePipe<PlanarImage> pipeSaveFromOpening = new SimplePipe<>((Readable) openingFilter);
+
+                SaveFilter saveFilter = new SaveFilter((Readable) pipeSaveFromOpening);
+                SimplePipe<PlanarImage> pipeCalcCentroidsFromSave = new SimplePipe<>((Readable) saveFilter);
+
+                CalcCentroidsFilter calcCentroidsFilter = new CalcCentroidsFilter((Readable) pipeCalcCentroidsFromSave);
+                SimplePipe<ArrayList<Coordinate>> pipeQualityFromCalcCentroids = new SimplePipe<>((Readable)calcCentroidsFilter);
+
+                QualityControllFilter qualityFilter = new QualityControllFilter(xTolerance, yTolerance, expectedCoordinates, pipeQualityFromCalcCentroids);
+                qualityFilter.run();
+                return;
+            }
+        }
     }
 }
--- src/imaging/filter/QualityControllFilter.java
+++ src/imaging/filter/QualityControllFilter.java
@@ -18,36 +18,64 @@ public class QualityControllFilter extends Sink<ArrayList<Coordinate>> {
 
     int xTolerance;
     int yTolerance;
+    ArrayList<Coordinate> expectedCoordinates;
 
-    public QualityControllFilter(int xTolerance, int yTolerance) throws InvalidParameterException {
+    public QualityControllFilter(int xTolerance, int yTolerance, ArrayList<Coordinate> expectedCoordinates) throws InvalidParameterException {
         super();
         this.xTolerance = xTolerance;
         this.yTolerance = yTolerance;
+        this.expectedCoordinates = expectedCoordinates;
     }
 
-    public QualityControllFilter(int xTolerance, int yTolerance, Readable<ArrayList<Coordinate>> input) throws InvalidParameterException {
+    public QualityControllFilter(int xTolerance, int yTolerance, ArrayList<Coordinate> expectedCoordinates, Readable<ArrayList<Coordinate>> input) throws InvalidParameterException {
         super(input);
         this.xTolerance = xTolerance;
         this.yTolerance = yTolerance;
+        this.expectedCoordinates = expectedCoordinates;
     }
 
     @Override
     public void write(ArrayList<Coordinate> coordinates) throws StreamCorruptedException {
-
         if(coordinates == null) {
             return;
         }
 
         File outputFile = new File("Outputfiles", "tolerances.txt");
 
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile)))
-        {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            bw.write("Auswertung Bildverarbeitung" + System.lineSeparator() + System.lineSeparator());
+            bw.write("Es wurden " + coordinates.size() + " Scheiben gefunden."  + System.lineSeparator());
+            bw.write("Gefundene Zentren: "  + System.lineSeparator());
+
             for(Coordinate coordinate : coordinates) {
-                bw.write("x: " + coordinate._x + ", y: " + coordinate._y + System.lineSeparator());
+                bw.write("x: " + coordinate._x + "\ty: " + coordinate._y + System.lineSeparator());
+            }
+
+            bw.write(System.lineSeparator() + System.lineSeparator());
+            bw.write("Max. x-Toleranz: " + xTolerance + System.lineSeparator());
+            bw.write("Max. y-Toleranz: " + yTolerance + System.lineSeparator() + System.lineSeparator());
+
+            boolean xValid = false;
+            boolean yValid = false;
+            for(int i = 0; i < coordinates.size(); i++) {
+
+                if(Math.abs(expectedCoordinates.get(i)._x - coordinates.get(i)._x) > xTolerance) {
+                    xValid = false;
+                }else {
+                    xValid = true;
+                }
+
+                if(Math.abs(expectedCoordinates.get(i)._y - coordinates.get(i)._y) > yTolerance) {
+                    yValid = false;
+                }else {
+                    yValid = true;
+                }
+
+                bw.write(i+1 + ". Scheibe:\tx-Soll: " + expectedCoordinates.get(i)._x + "\tx-Ist: " + coordinates.get(i)._x + "\t"+ (xValid?"OK" : "FAIL") + System.lineSeparator());
+                bw.write(i+1 + ". Scheibe:\ty-Soll: " + expectedCoordinates.get(i)._y + "\ty-Ist: " + coordinates.get(i)._y + "\t"+ (yValid?"OK" : "FAIL") + System.lineSeparator());
             }
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             e.printStackTrace();
         }
     }
